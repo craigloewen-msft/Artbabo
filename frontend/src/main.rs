849bb81546806b251b58fbb0090ce4433d64bcd3
Craig Loewen <crloewen@microsoft.com>
2024-11-03 13:22:30 +0000

    Finalized switch

---
+++ frontend/src/main.rs
@@ -0,0 +1,35 @@
+use bevy::{prelude::*, render::camera::ScalingMode};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                // fill the entire browser window
+                fit_canvas_to_parent: true,
+                // don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .insert_resource(ClearColor(Color::srgb(0.53, 0.53, 0.53))) 
+        .add_systems(Startup, (setup, spawn_player)) 
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    let mut camera_bundle = Camera2dBundle::default();
+    camera_bundle.projection.scaling_mode = ScalingMode::FixedVertical(10.);
+    commands.spawn(camera_bundle);
+}
+
+fn spawn_player(mut commands: Commands) {
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::srgb(0., 0.47, 1.),
+            custom_size: Some(Vec2::new(1., 1.)),
+            ..default()
+        },
+        ..default()
+    });
+}
